a293b16e5694bab45274dd8531286ce141beefb0
whatdaahaael <sujanedwin2006@gmail.com>
2025-12-04 00:14:46 +0530

    Changed Ordering.\nAdded 3 Programs:\nCharacter frequency count in string(P05),\nDuplicate element remove in string(P06),\nReverse string using slice and loops(P07).

---
+++ P06_DupStrElmntRm.py
@@ -0,0 +1,13 @@
+
+def removeDupEl(word:str):
+    newWord=""
+    for ch in word:
+        if ch not in newWord:
+            newWord+=ch
+    return newWord
+
+strList=["banana", "apple", "mango", "strawberry"]
+for i in range(0, len(strList)):
+    strList[i]=removeDupEl(strList[i])
+
+print(strList)
---
+++ P07_ReverseString.py
@@ -0,0 +1,10 @@
+
+word=input("Enter string: ")
+
+revWord=""
+for ch in word:
+    revWord=ch+revWord
+
+print(f"Orginal Word: {word}")
+print(f"Slice Reversed: {word[::-1]}")
+print(f"Loop Reversed: {revWord}")
