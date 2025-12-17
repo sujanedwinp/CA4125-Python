84034dd91c4d9ec359ee1a63101b4771d5b50aca
whatdaahaael <sujanedwin2006@gmail.com>
2025-12-18 00:57:42 +0530

    Added Stack and Queue DS with mh menu.

--- P09_queue.py
+++ P09_queue.py
@@ -1,47 +1,47 @@
 
-class Stack:
+class Queue:
     def __init__(self):
-        self.st=[]
+        self.q=[]
 
-    def pushStack(self, item):
-        self.st.append(item)
+    def enqueue(self, item):
+        self.q.append(item)
 
-    def popStack(self):
+    def dequeue(self):
         if self.isEmpty():
-            print("Nothing to Remove.")
+            print("Nothing to Dequeue.")
             return None
-        return self.st.pop()
+        return self.q.pop(0)
     
-    def showStack(self):
+    def showQueue(self):
         if self.isEmpty():
             print("Nothing to Show.")
             return None
-        for item in reversed(self.st):
+        for item in self.q:
             print(item)
     
     def size(self):
-        return len(self.st)
+        return len(self.q)
     
     def isEmpty(self):
         if self.size()==0:
             return True
         return False
 
-stack=Stack()
+queue=Queue()
 
 while(True):
     print("\nMenu.")
-    choice=int(input("1. Push Item\n2. Pop Item\n3. Show Stack\n4. Size of Stack\n5. Exit\n"))
+    choice=int(input("1. Enqueue\n2. Dequeue\n3. Show Queue\n4. Size of Queue\n5. Exit\n"))
     if choice==1:
         item=input("Enter Item: ")
-        stack.pushStack(item)
+        queue.enqueue(item)
     elif choice==2:
-        print(f"Item Removed: {stack.popStack()}")
+        print(f"Item Removed: {queue.dequeue()}")
     elif choice==3:
-        print("Stack")
-        stack.showStack()
+        print("Queue:")
+        queue.showQueue()
     elif choice==4:
-        print(f"Size: {stack.size()}")
+        print(f"Size: {queue.size()}")
     elif choice==5:
         print("Exited")
         break
